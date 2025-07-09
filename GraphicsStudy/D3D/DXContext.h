6f3c53f7b98be6ab1e77509a99f2ea7d0fc6f92b
Kyungnam Kim <tractatuz@gmail.com>
2025-07-10 08:05:18 +0900

    D3D12 Beginners Tutorial 4 Fence & Command List

--- GraphicsStudy/D3D/DXContext.h
+++ GraphicsStudy/D3D/DXContext.h
@@ -9,6 +9,10 @@ public:
 	bool Init();
 	void ShutDown();
 
+	void SignalAndWait();
+	ID3D12GraphicsCommandList7* InitCommandList();
+	void ExecuteCommandList();
+
 	inline ComPointer<ID3D12Device10>& GetDevice()
 	{
 		return m_device;
@@ -22,8 +26,13 @@ public:
 private:
 	ComPointer<ID3D12Device10> m_device;
 	ComPointer<ID3D12CommandQueue> m_cmdQueue;
+
+	ComPointer<ID3D12CommandAllocator> m_cmdAllocator;
+	ComPointer<ID3D12GraphicsCommandList7> m_cmdList;
+
 	ComPointer<ID3D12Fence1> m_fence;
 	UINT64 m_fenceValue = 0;
+	HANDLE m_fenceEvent = nullptr;
 
 	// Singleton
 public:
