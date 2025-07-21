00e4b66edc6709922a7c41b8fc395c182acd27b7
tractatuz <tractatuz@gmail.com>
2025-07-21 20:16:10 +0900

    Add Nuget package (Microsoft.Direct3D.D3D12)

--- GraphicsStudy/main.cpp
+++ GraphicsStudy/main.cpp
@@ -7,6 +7,17 @@
 #include <Debug/DXDebugLayer.h>
 #include <D3D/DXContext.h>
 
+extern "C" {
+	// Setup to use the D3D12Core.dll which is distributed with this.
+	// 616 is Agility SDK version.
+	// Recommend to check the installed SDK header (D3D12.h) for the latest version number and use it.
+	__declspec(dllexport) extern const UINT D3D12SDKVersion = 616;
+
+	// Setup SDK DLL file path. 
+	// NuGet package automatically copies the DLLs to a "D3D12" folder next to the .exe file.
+	__declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\";
+}
+
 int main()
 {
 	DXDebugLayer::Get().Init();
